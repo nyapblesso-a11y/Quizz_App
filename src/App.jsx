70640bb803bfa57eb00a24213cf067d85cbd5344
nyapblesso-a11y <nyapblesso@gmail.com>
2025-11-22 10:14:46 +0100

    the home page was built and the stylings made to suitable for confort by the user

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HopePage from './HomePage/HomePage'
 
 function App() {
 
@@ -9,6 +8,7 @@ function App() {
   return (
     <>
       <div>
+        <HopePage/>
       </div>
     </>
   )
---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,21 @@
+import "./HomePage.css";
+
+const HopePage = () => {
+  return (
+    <>
+     <div className="bg-transparent  p-8 rounded-lg shadow-md text-center">
+          <h1 className="text-4xl font-bold text-violet mb-6">
+            Welcome to the Quiz!
+          </h1>
+          <p className="text-lg text-gray-900 mb-8">
+            Test your knowledge and have fun.
+          </p>
+          <button className="bg-transparent p-8 hover:bg-[#232024] text-white font-bold py-3 px-6 rounded-1.5xl text-xl transition duration-300 ease-in-out transform hover:scale-105">
+            Start Quiz
+          </button>
+        </div>
+    </>
+  );
+};
+
+export default HopePage;
