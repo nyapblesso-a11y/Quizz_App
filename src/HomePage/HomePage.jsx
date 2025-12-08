fba65dbe1f5c8171b5fe58cf65366af27cb8bf22
nyapblesso-a11y <nyapblesso@gmail.com>
2025-12-08 10:01:45 +0100

    the rules of the game has been stated to ensure that the user knows what the game is all about

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -2,26 +2,32 @@ import { Navigate, useNavigate } from "react-router-dom";
 import { useQuesStore } from "../Store/quizzStore";
 
 const HopePage = () => {
-  const navigate = useNavigate()
-  const { startQuizz} = useQuesStore()
+  const navigate = useNavigate();
+  const { startQuizz } = useQuesStore();
   const handleStart = async () => {
-    await startQuizz()
-    navigate('/quizz')
-  }
+    await startQuizz();
+    navigate("/quizz");
+  };
   return (
     <>
-     <div className="bg-transparent  p-8 rounded-lg shadow-md text-center">
-          <h1 className="text-4xl font-bold text-amber-100 mb-6">
-            Welcome to the Quiz!
-          </h1>
-          <p className="text-2xl text-white mb-8">
-            Test your knowledge and have fun.
-          </p>
-           <h6 className="text-2xl text-gray-900 mb-2 font-bold ">You've got 10 seconds to answer each question have fun</h6>
-          <button className="bg-transparent p-8 hover:bg-[#232024] text-amber-100 font-bold py-3 px-6 rounded-1.5xl text-xl transition duration-300 ease-in-out transform hover:scale-105" onClick={handleStart}>
-            Start Quiz
-          </button>
-        </div>
+      <div className="bg-transparent  p-8 rounded-lg shadow-md text-center">
+        <h1 className="text-4xl font-bold text-amber-100 mb-6">
+          Welcome to the Quiz!
+        </h1>
+        
+        <h6 className="text-2xl text-gray-900 mb-2 font-bold ">
+          Game Rules: You've got 10 random questions to answer in 10 seconds.
+          Failure to answer any question will automatically take you to the next
+          question and then you'll score no point to the unanswered question.
+          Test your knowledge and have fun.
+        </h6>
+        <button
+          className="bg-transparent p-8 hover:bg-[#232024] text-amber-100 font-bold py-3 px-6 rounded-1.5xl text-xl transition duration-300 ease-in-out transform hover:scale-105"
+          onClick={handleStart}
+        >
+          Start Quiz
+        </button>
+      </div>
     </>
   );
 };
