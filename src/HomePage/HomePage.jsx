64c683b3a8e7714e3421528616dc2666a8c8d91e
nyapblesso-a11y <nyapblesso@gmail.com>
2025-12-10 13:59:08 +0100

    the background color of both the buttton and the page was change alongside the color of text all change to white

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -11,18 +11,18 @@ const HopePage = () => {
   return (
     <>
       <div className="bg-transparent  p-8 rounded-lg shadow-md text-center">
-        <h1 className="text-4xl font-bold text-amber-100 mb-6">
+        <h1 className="text-4xl font-bold text-white m-6">
           Welcome to the Quiz!
         </h1>
         
-        <h6 className="text-2xl text-gray-900 mb-2 font-bold ">
+        <h6 className="text-2xl text-white mb-2 font-bold ">
           Game Rules: You've got 10 random questions to answer in 10 seconds
           Failure to answer any question will automatically take you to the next
           question and then you'll score no point to the unanswered question.
           Test your knowledge and have fun.
         </h6>
         <button
-          className="bg-transparent p-8 hover:bg-[#232024] text-amber-100 font-bold py-3 px-6 rounded-1.5xl text-xl transition duration-300 ease-in-out transform hover:scale-105"
+          className="bg-transparent border-2 p-8 hover:bg-[#232024] text-white font-bold py-3 px-6 rounded-1.5xl text-xl transition duration-300 ease-in-out transform hover:scale-105"
           onClick={handleStart}
         >
           Start Quiz
